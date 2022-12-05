de648b3d0d4412428114a639f3528c9dd3d56d9b
mmbacho <philip.kjellander@skola.taby.se>
2022-12-05 09:25:02 +0100

    stats

--- game.py
+++ game.py
@@ -1,20 +1,16 @@
 print(" Adventure Boys: The game")
 
-class stats():
+class playerstats():
 
-    def stats(Zacke):
-        Zacke.HP = 50
-        Zacke.S = 18
-        Zacke.AC = 10
+    def __init__(self, name_in, hp_in, strength_in, armorclass_in):
+        self.name = name_in
+        self.hp = hp_in
+        self.strength = strength_in
+        self.armorclass = armorclass_in
 
-    def stats(Jacke):
-        Jacke.HP = 70
-        Jacke.S = 10
-        Jacke.AC = 15
+    def print_info(self):
+        print(f"Name: {self.name} Healthpool: {self.hp} Strength: {self.strength} Armorclass: {self.armorclass}")
 
-    def stats(Wilson):
-        Wilson.HP = 55
-        Wilson.S = 14
-        Wilson.AC = 12
-
-   
+Zacke_Swiftfoot = stats("Zacke Swiftfoot", 50, 18, 10)
+Jacke_Bigfoot = stats("Jacke Slowfoot", 70, 10, 15)
+Wilson_Slowfoot = stats("Wilson Bigfoot", 55, 14, 12)
