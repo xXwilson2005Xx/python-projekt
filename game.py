946a6521a43eff1d409be981b91a4d721e4737bc
mmbacho <philip.kjellander@skola.taby.se>
2022-12-05 09:06:01 +0100

    fixed

--- game.py
+++ game.py
@@ -2,17 +2,17 @@ print(" Adventure Boys: The game")
 
 class stats():
 
-    def __init__(Zacke Swiftfoot) -> None:
+    def stats(Zacke):
         Zacke.HP = 50
         Zacke.S = 18
         Zacke.AC = 10
 
-    def __init__(Jacke Bigfoot) -> None:
+    def stats(Jacke):
         Jacke.HP = 70
         Jacke.S = 10
         Jacke.AC = 15
 
-    def __init__(Wilson Slowfoot) -> None:
+    def stats(Wilson):
         Wilson.HP = 55
         Wilson.S = 14
         Wilson.AC = 12
