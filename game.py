9e122a4ca117602a4e8528ad2286fafb959a7793
mmbacho <philip.kjellander@skola.taby.se>
2022-12-05 15:15:50 +0100

    Män

--- game.py
+++ game.py
@@ -1,4 +1,10 @@
-print(" Adventure Boys: The game")
+x = int(input("Start game! 1.YES or 2.NO: "))
+if x == 1:
+    print("Adventure Boys: The game!")
+else:
+    print("Try again later!")
+
+
 
 class playerstats():
 
