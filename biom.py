4f87108df2eb750d5e4c0f7de2333ec2f7ad82f0
ZACISEN <axel.scholin@skola.taby.se>
2022-12-08 09:23:10 +0100

    stav fel

--- biom.py
+++ biom.py
@@ -1,12 +1,12 @@
 print("1.Mountain,N \n2.Desert,S \n3.Jungle,E \n4.Plains,W")
 
-chosebiom=int(input("\nchose a diration(nr)"))
-if chosebiom == 1:
+choosebiom=int(input("\nchoose a diration(nr)"))
+if choosebiom == 1:
     biom = "mountain"
-elif chosebiom == 2:
+elif choosebiom == 2:
     biom = "Desert"
-elif chosebiom == 3:
+elif choosebiom == 3:
     biom = "Jungle"
-elif chosebiom == 4:
+elif choosebiom == 4:
     biom = "Plains"  
 print(f"You whent thordes the{biom}") 
