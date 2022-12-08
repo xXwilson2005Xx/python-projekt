6e1a98bc1fde95ae5cfb111b14535520b4640f0b
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2022-12-08 09:35:50 +0100

    title

--- game.py
+++ game.py
@@ -1,3 +1,9 @@
+from art import*
+
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n")
+printtitlescreen()
+print("\n\n\n")
+
 Start_the_game = int(input("Start game! 1.YES or 2.NO: "))
 if Start_the_game == 1:
     print("Adventure Boys: The game!")
