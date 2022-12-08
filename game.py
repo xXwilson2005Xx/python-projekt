8da029d5733bfbc89b3e77a9777c333641ccb6f9
mmbacho <philip.kjellander@skola.taby.se>
2022-12-08 09:17:07 +0100

    mumma

--- game.py
+++ game.py
@@ -1,7 +1,5 @@
-x = int(input("Start game! 1.YES or 2.NO: "))
-if x == 1:
+Start_the_game = int(input("Start game! 1.YES or 2.NO: "))
+if Start_the_game == 1:
     print("Adventure Boys: The game!")
 else:
     print("Try again later!")
-
-
