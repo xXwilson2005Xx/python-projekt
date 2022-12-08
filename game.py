b784af9f4185578be9758ede9fa116c0ce16e837
mmbacho <philip.kjellander@skola.taby.se>
2022-12-08 09:01:34 +0100

    stats bort

--- game.py
+++ game.py
@@ -5,18 +5,3 @@ else:
     print("Try again later!")
 
 
-
-class playerstats():
-
-    def __init__(self, name_in, hp_in, strength_in, armorclass_in):
-        self.name = name_in
-        self.hp = hp_in
-        self.strength = strength_in
-        self.armorclass = armorclass_in
-
-    def print_info(self):
-        print(f"Name: {self.name} Healthpool: {self.hp} Strength: {self.strength} Armorclass: {self.armorclass}")
-
-Zacke_Swiftfoot = playerstats("Zacke Swiftfoot", 50, 18, 10)
-Jacke_Bigfoot = playerstats("Jacke Slowfoot", 70, 10, 15)
-Wilson_Slowfoot = playerstats("Wilson Bigfoot", 55, 14, 12)
