2954fe83d87718b6bb2f845cd2b74e1e2286570e
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2022-12-13 10:47:47 +0100

    funktion biom

--- biom.py
+++ biom.py
@@ -1,15 +1,18 @@
 print("1.Mountain,N \n2.Desert,S \n3.Jungle,E \n4.Plains,W")
+def biomer():
+    choosebiom=int(input("\nChoose a diraction(nr): "))
+    if choosebiom == 1:
+        biom = "Mountains"
+    elif choosebiom == 2:
+        biom = "Desert"
+    elif choosebiom == 3:
+        biom = "Jungle" 
+    elif choosebiom == 4:
+        biom = "Plains"  
+    else:
+            print("Your stupid shithead write 1, 2, 3 or 4!")
+    return biom
 
-choosebiom=int(input("\nchoose a diration(nr): "))
-if choosebiom == 1:
-    biom = "mountain"
-elif choosebiom == 2:
-    biom = "Desert"
-elif choosebiom == 3:
-    biom = "Jungle"
-elif choosebiom == 4:
-    biom = "Plains"  
-else:
-    print("Your stupid shithead write 1, 2, 3 or 4!")
+biomHeading = biomer()
 
-print(f"You went towards the {biom}") 
+print(f"You went towards the {biomHeading}") 
