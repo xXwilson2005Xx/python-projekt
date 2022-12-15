928fae91f28db39e5e3471e68b734a0d09b62698
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2022-12-15 09:05:54 +0100

    level

--- playerstats.py
+++ playerstats.py
@@ -1,17 +1,18 @@
 class playerstats():
 
-    def __init__(self, name_in, hp_in, strength_in, armorclass_in):
+    def __init__(self, name_in, hp_in, strength_in, armorclass_in, level_in):
         self.name = name_in
         self.hp = hp_in
         self.strength = strength_in
         self.armorclass = armorclass_in
+        self.level = level_in
 
     def print_info(self):
         print(f"Name: {self.name} Healthpool: {self.hp} Strength: {self.strength} Armorclass: {self.armorclass}")
 
-Zacke_Swiftfoot = playerstats("Zacke Swiftfoot", 50, 18, 10)
-Jacke_Bigfoot = playerstats("Jacke Bigfoot", 70, 12, 15)
-Wilson_Slowfoot = playerstats("Wilson Slowfoot", 55, 15, 12)
+Zacke_Swiftfoot = playerstats("Zacke Swiftfoot", 50, 18, 10, 0)
+Jacke_Bigfoot = playerstats("Jacke Bigfoot", 70, 12, 15, 0)
+Wilson_Slowfoot = playerstats("Wilson Slowfoot", 55, 15, 12, 0)
 
 class mobstats():
 
