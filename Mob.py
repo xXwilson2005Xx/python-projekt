f7db26e5f54b51c4ae6b50c59ccd6af71630db48
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2022-12-15 09:10:50 +0100

    Added levels

--- Mob.py
+++ Mob.py
@@ -3,7 +3,7 @@ def mobaction():
     import random as rand
     mobchoise = rand.randint(1, 2)
     if mobchoise == 1:
-        print("attck")
+        print("attack")
     else:
         print("defend")
     return mobchoise
