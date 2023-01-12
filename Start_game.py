4ecb22ecf17a84bd45552a195f326d9a0cc8fee1
ZACISEN <axel.scholin@skola.taby.se>
2023-01-12 09:27:36 +0100

    start

--- Start_game.py
+++ Start_game.py
@@ -1,11 +1,21 @@
 def Start_game():
-    Start_the_game = int(input("Start game! 1.YES or 2.NO: "))
-    if Start_the_game == 1:
-        print("""
+    try:
+        Start_the_game = int(input("Start game! 1.YES or 2.NO: "))
+        if Start_the_game == 1:
+                print("""
         <-------------------->
         Martin Boys: The game!
         <-------------------->
         """)
-    elif Start_the_game == 2:
-       print("Closing...")
-       exit()
+        elif Start_the_game == 2:
+                print("Closing...")
+                exit()
+    except ValueError:
+        print("try a number")
+        Start_game()
+        
+
+
+    
+
+
