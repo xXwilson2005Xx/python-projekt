d6addbcabcc11759465dcd675cd060ee9f721cd2
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2023-01-16 09:44:25 +0100

    mobaction

--- Mob.py
+++ Mob.py
@@ -1,20 +1,20 @@
+from playerstats import*
 
-def mobaction():
+
+def mobaction(player):
     import random as rand
     mobchoice = rand.randint(1, 2)
     if mobchoice == 1:
-        print("attack")
+        print("Attack")
+        import random as rand
+        hittarget = rand.randint(1, 20)
+        if hittarget <= player.armorclass:
+            print("Attack succeded!")
+        else:
+            print("Attack faild!")
     else:
-        print("defend")
+        print("Defend")
     return mobchoice
 
 
-
-def mob_hitchance():
-    import random as rand
-    hittarget = rand.randint(1, 20)
-    print(hittarget)
-    return hittarget
-
-    
-mob_hitchance()
+mobaction()
