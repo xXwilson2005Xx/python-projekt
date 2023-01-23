2b4a9f568cd5931d7a78e8593162ed8a4510b99e
mmbacho <philip.kjellander@skola.taby.se>
2023-01-23 09:23:30 +0100

    player comabt fix

--- player_combat.py
+++ player_combat.py
@@ -1,11 +1,20 @@
 import random as rand
+from Mob import*
 from playerstats import*
 
-def player_combat(player, mob):
+def player_combat(player: playerstats, mob: mobstats):
     try:
         player_choice = int(input("Do you want to Attack or Defend? 1:Attack 2:Defend: "))
+        
         if player_choice == 1:
             print("Attack")
+            import random as rand
+            hittarget = rand.randint(1, 20)
+            if hittarget <= mob.armorclass:
+                mob.hp = mob.hp - player.strength
+                print(f'Your succeded, now the monster has',(mob.hp),'health')
+            else:
+                print("Attack faild!")
         else:
             print("Defend")
         return player_choice
