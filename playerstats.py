29cc9691d2dc0f0109846594c52dceb743cb9218
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2023-01-26 09:27:26 +0100

    Max Hp

--- playerstats.py
+++ playerstats.py
@@ -1,8 +1,9 @@
 class playerstats():
 
-    def __init__(self, name_in, hp_in, strength_in, armorclass_in, level_in):
+    def __init__(self, name_in, hp_in, maxhp_in, strength_in, armorclass_in, level_in):
         self.name = name_in
         self.hp = hp_in
+        self.maxhp = maxhp_in
         self.strength = strength_in
         self.armorclass = armorclass_in
         self.level = level_in
@@ -10,6 +11,6 @@ class playerstats():
     def print_info(self):
         print(f"Name: {self.name} Healthpool: {self.hp} Strength: {self.strength} Armorclass: {self.armorclass} Level: {self.level}")
 
-Zacke_Swiftfoot = playerstats("Zacke Swiftfoot", 50, 18, 10, 1)
-Jacke_Bigfoot = playerstats("Jacke Bigfoot", 70, 12, 15, 1) 
-Wilson_Slowfoot = playerstats("Wilson Slowfoot", 55, 15, 12, 1)
+Zacke_Swiftfoot = playerstats("Zacke Swiftfoot", 50, 50, 18, 10, 1)
+Jacke_Bigfoot = playerstats("Jacke Bigfoot", 70, 70, 12, 15, 1) 
+Wilson_Slowfoot = playerstats("Wilson Slowfoot", 55, 55, 15, 12, 1)
