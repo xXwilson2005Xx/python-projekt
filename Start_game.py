4cfb9c5b6b3582e2b92667dea9b9ffad26e8e2fd
xXwilson2005Xx <benjamin.wilson@skola.taby.se>
2023-01-27 10:45:39 +0100

    added lore

--- Start_game.py
+++ Start_game.py
@@ -3,15 +3,31 @@ def Start_game():
         Start_the_game = int(input("Start game! 1.YES or 2.NO: "))
         if Start_the_game == 1:
             print("""
-        <-------------------->
-        Martin Boys: The game!
-        <-------------------->
-        """)
+            <--------------------------------------------------------------------------------->
+             The world has been ridden in darkness and only one can save it. 
+             You, the noble hero, have to save the Lord, Martin. 
+             You are one of his devoted knights and have been bagged by the world to save them. 
+             You must bring out light in each region of darkness.
+            <--------------------------------------------------------------------------------->
+            """)
+            print("""
+            <-------------------->
+            Martin Boys: The game!
+            <-------------------->
+            """)  
+
         elif Start_the_game != 1:
             
             print("""
         Too bad, you are playing this anyway!
-        
+
+        <--------------------------------------------------------------------------------->
+         The world has been ridden in darkness and only one can save it. 
+         You, the noble hero, have to save the Lord, Martin. 
+         You are one of his devoted knights and have been bagged by the world to save them. 
+         You must bring out light in each region of darkness.
+        <--------------------------------------------------------------------------------->
+  
         <-------------------->
         Martin Boys: The game!
         <-------------------->
